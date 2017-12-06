f45cecac903915c3d9db6c9d67e562e5d58e0185
Jamal A. Williams <jamalw@della5.princeton.edu>
2017-12-06 11:38:41 -0500

    full script ready

--- searchlight_k_sweep_srm.py
+++ searchlight_k_sweep_srm.py
@@ -6,10 +6,10 @@ import sys
 from brainiak.funcalign.srm import SRM
 import nibabel as nib
 
-#subjs = ['MES_022817_0','MES_030217_0','MES_032117_1','MES_040217_0','MES_041117_0','MES_041217_0','MES_041317_0','MES_041417_0','MES_041517_0','MES_042017_0','MES_042317_0','MES_042717_0','MES_050317_0','MES_051317_0','MES_051917_0','MES_052017_0','MES_052017_1','MES_052317_0','MES_052517_0','MES_052617_0','MES_052817_0','MES_052817_1','MES_053117_0','MES_060117_0','MES_060117_1']
+subjs = ['MES_022817_0','MES_030217_0','MES_032117_1','MES_040217_0','MES_041117_0','MES_041217_0','MES_041317_0','MES_041417_0','MES_041517_0','MES_042017_0','MES_042317_0','MES_042717_0','MES_050317_0','MES_051317_0','MES_051917_0','MES_052017_0','MES_052017_1','MES_052317_0','MES_052517_0','MES_052617_0','MES_052817_0','MES_052817_1','MES_053117_0','MES_060117_0','MES_060117_1']
 
-subjs = ['MES_022817_0','MES_030217_0','MES_032117_1']
-k_sweeper = np.arange(2,4)
+#subjs = ['MES_022817_0','MES_030217_0','MES_032117_1']
+k_sweeper = np.arange(3,10)
 
 loo_idx = int(sys.argv[1])
 subj = subjs[int(loo_idx)]
@@ -103,7 +103,7 @@ def HMM(X,K,loo_idx):
 
     """
     
-    w = 5
+    w = 2
     nPerm = 1000
     within_across = np.zeros(nPerm+1)
     run1 = [X[i] for i in np.arange(0, int(len(X)/2))]
