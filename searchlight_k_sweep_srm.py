60e06656c5cc348a580721b69e9d3fd095b94fbe
Jamal A. Williams <jamalw@della5.princeton.edu>
2017-12-11 11:07:29 -0500

    latest version for chris to look at. script keeps timing out after 3 days

--- searchlight_k_sweep_srm.py
+++ searchlight_k_sweep_srm.py
@@ -9,7 +9,7 @@ import nibabel as nib
 subjs = ['MES_022817_0','MES_030217_0','MES_032117_1','MES_040217_0','MES_041117_0','MES_041217_0','MES_041317_0','MES_041417_0','MES_041517_0','MES_042017_0','MES_042317_0','MES_042717_0','MES_050317_0','MES_051317_0','MES_051917_0','MES_052017_0','MES_052017_1','MES_052317_0','MES_052517_0','MES_052617_0','MES_052817_0','MES_052817_1','MES_053117_0','MES_060117_0','MES_060117_1']
 
 #subjs = ['MES_022817_0','MES_030217_0','MES_032117_1']
-k_sweeper = np.arange(3,10)
+k_sweeper = [3,6,9,12]
 
 loo_idx = int(sys.argv[1])
 subj = subjs[int(loo_idx)]
